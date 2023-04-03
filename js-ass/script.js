3ab2e8606ba04aea58c0f7695f2bd44735ec5039
leestar0629 <yale901@naver.com>
2023-04-03 17:47:08 +0900

    js 과제

--- js-ass/script.js
+++ js-ass/script.js
@@ -65,9 +65,23 @@ const checkGuess = () => {
 
 
   guesses.textContent += userGuess + ' ';
-  while()
-  // 입력값에 따라 , isLow, isHigh, Collect 등의 함수를 호출 
-  // guess Count가 10이 될 경우 game over
+  if(guessCount==10){
+    gameOver()
+    resetGame()
+  }
+  
+  while(guessCount!=10){
+    if(userGuess<randomNumber){
+      isLow();
+    }
+    else if(userGuess>randomNumber){
+      isHigh();
+    }
+    else{
+      collect();
+    }
+  }
+  
   guessCount++;
   guessField.value = '';
   guessField.focus();
